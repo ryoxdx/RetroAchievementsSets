096da83fd995d17179950cacafe4fb7edcc93cb4
ryoxdx <198821000+ryoxdx@users.noreply.github.com>
2026-07-14 00:55:25 -0300

    fix: add andnext on the nopenmissed trigger

--- PS2/FIFA14/main.js
+++ PS2/FIFA14/main.js
@@ -685,19 +685,10 @@ const codeFor = () => {
     ['', 'Mem', '32bit', 0x04, '=', 'Value', '', 6],
   );
 
+  // prettier-ignore
   const homePenNoMissed = $(
-    ['', 'Delta', '32bit', addresses.penaltyHome1, '=', 'Value', '', 0],
-    [
-      'Trigger',
-      'Mem',
-      '32bit',
-      addresses.penaltyHome1,
-      '!=',
-      'Value',
-      '',
-      0,
-      1,
-    ],
+    ['AndNext', 'Delta', '32bit', addresses.penaltyHome1, '=', 'Value', '', 0],
+    ['Trigger', 'Mem', '32bit', addresses.penaltyHome1, '!=', 'Value', '', 0, 1],
     ['ResetIf', 'Mem', '32bit', addresses.penaltyHome1, '=', 'Value', '', 1],
     ['ResetIf', 'Mem', '32bit', addresses.penaltyHome2, '=', 'Value', '', 1],
     ['ResetIf', 'Mem', '32bit', addresses.penaltyHome3, '=', 'Value', '', 1],
